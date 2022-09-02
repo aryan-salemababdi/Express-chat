d71d264f9bbcec4e94badd17f8d60d7657e73803
Aryan Salemabadi <aryanavatar1380@gmail.com>
2022-09-02 20:33:50 +0430

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const io = require("socket.io");
+const app = express();
+app.get("/" , (req,res) =>{
+    res.send(" salam i'm aryan ");
+});
+const server = app.listen(3010,err =>{
+    console.log("app listen to port 3010");
+})
+const socket = io(server);
+const mySocket = socket.of("/socket");
+
+
+mySocket.on("connection",(socket)=>{
+    console.log("new User Connected");
+
+    socket.on("newMessage" , (message) =>{
+        console.log(message.msg);
+        mySocket.emit("havemessage" , message);
+    }
+    )
+
+    socket.on("disconnect",()=>{
+        console.log("User disconnected");
+    })
+});
